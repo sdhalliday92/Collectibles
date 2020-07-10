35e706f2fc6f475e21b06fc2790a41b6b3b29d2d
sdhalliday92 <sdhalliday92@gmail.com>
2020-07-10 11:05:22 +0100

    Added login functionality

--- app.py
+++ app.py
@@ -1,10 +1,15 @@
-from flask import Flask, redirect, url_for
-from flask import render_template
+from flask import Flask, redirect, url_for, render_template
 from flask_sqlalchemy import SQLAlchemy
+from flask_bcrypt import Bcrypt
 from os import environ
-from forms import CollectibleForm
+from forms import CollectibleForm, RegistrationForm, LoginForm, UpdateAccountForm
+from flask_login import UserMixin, LoginManager, login_user, logout_user, login_required, current_user
+from datetime import datetime
+from wtforms.validators import ValidationError
+
 
 app = Flask(__name__)
+bcrypt = Bcrypt(app)
 
 app.config['SECRET_KEY'] = '211c8c0e484b5eec9d582727aefbf64e'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
@@ -21,19 +26,43 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://' + \
 
 db = SQLAlchemy(app)
 
+login_manager = LoginManager(app)
+login_manager.login_view = 'login'
+
 
 class Collectibles(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(100), nullable=False)
     cat = db.Column(db.String(100), nullable=False)
+    date_posted = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
 
     def __repr__(self):
-        return ''.join(
-            [
-                'Name: ' + self.name + '\n'
-                                       'Category: ' + self.cat
-            ]
-        )
+        return ''.join([
+            'User ID: ', self.user_id, '\r\n',
+            'Name: ', self.name, '\r\n', self.cat
+        ])
+
+
+class Users(db.Model, UserMixin):
+    id = db.Column(db.Integer, primary_key=True)
+    first_name = db.Column(db.String(30), nullable=False)
+    last_name = db.Column(db.String(30), nullable=False)
+    email = db.Column(db.String(150), nullable=False, unique=True)
+    password = db.Column(db.String(250), nullable=False)
+    post = db.relationship('Posts', backref='author', lazy=True)
+
+    def __repr__(self):
+        return ''.join(['UserID: ', str(self.id), '\r\n',
+                        'Email: ', self.email], '\r\n',
+                       'Name: ', self.first_name, ' ', self.last_name
+                       )
+
+
+@login_manager.user_loader
+def load_user(id):
+    return Users.query.get(int(id))
 
 
 @app.route('/')
@@ -48,6 +77,83 @@ def about():
     return render_template('about.html', title='About')
 
 
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    if current_user.is_authenticated:
+        return redirect(url_for('home'))
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        hash_pw = bcrypt.generate_password_hash(form.password.data)
+        user = Users(
+            first_name=form.first_name.data,
+            last_name=form.last_name.data,
+            email=form.email.data,
+            password=hash_pw
+        )
+
+        db.session.add(user)
+        db.session.commit()
+
+        return redirect(url_for('post'))
+
+    return render_template('register.html', title='Register', form=form)
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if current_user.is_authenticated:
+        return redirect(url_for('home'))
+    form = LoginForm()
+    if form.validate_on_submit():
+        user = Users.query.filter_by(email=form.email.data).first()
+        if user and bcrypt.check_password_hash(user.password, form.password.data):
+            login_user(user, remember=form.remember.data)
+            next_page = request.args.get('next')
+            if next_page:
+                return redirect(next_page)
+            else:
+                return redirect('home')
+    return render_template('login.html', title='Login', form=form)
+
+
+@app.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    return redirect(url_for('login'))
+
+
+@app.route('/account', methods=['GET', 'POST'])
+@login_required
+def account():
+    form = UpdateAccountForm()
+    if form.validate_on_submit():
+        current_user.first_name = form.first_name.data
+        current_user.last_name = form.last_name.data
+        current_user.email = form.email.data
+        db.session.commit()
+        return redirect(url_for('account'))
+    elif request.method == 'GET':
+        form.first_name.data = current_user.first_name
+        form.last_name.data = current_user.last_name
+        form.email.data = current_user.email
+    return render_template('account.html', title='Account', form=form)
+
+
+@app.route("/account/delete", methods=["GET", "POST"])
+@login_required
+def account_delete():
+    user = current_user.id
+    collectibles = Collectibles.query.filter_by(user_id=user)
+    for entry in collectibles:
+        db.session.delete(entry)
+    account = Users.query.filter_by(id=user).first()
+    logout_user()
+    db.session.delete(account)
+    db.session.commit()
+    return redirect(url_for('register'))
+
+
 @app.route('/add', methods=['GET', 'POST'])
 def add():
     form = CollectibleForm()
@@ -60,7 +166,7 @@ def add():
         db.session.commit()
         return redirect(url_for('home'))
     else:
-        return render_template('post.html', title='Add Collectible', form=form)
+        return render_template('add_entry.html', title='Add Collectible', form=form)
 
 
 @app.route('/delete')
@@ -78,6 +184,13 @@ def create():
     db.session.add(post)
     db.session.commit()
     return redirect(url_for('home'))
+
+
+def validate_email(self, email):
+    if email.data != current_user.email:
+        user = Users.query.filter_by(email=email.data).first()
+        if user:
+            raise ValidationError('Email already in use')
 
 
 if __name__ == '__main__':
--- forms.py
+++ forms.py
@@ -1,6 +1,6 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField
-from wtforms.validators import DataRequired, Length
+from wtforms import StringField, SubmitField, PasswordField, BooleanField
+from wtforms.validators import DataRequired, Length, Email, EqualTo
 
 
 class CollectibleForm(FlaskForm):
@@ -21,3 +21,75 @@ class CollectibleForm(FlaskForm):
     )
 
     submit = SubmitField('Add Collectible')
+
+
+class RegistrationForm(FlaskForm):
+    first_name = StringField('First Name',
+                             validators=[
+                                 DataRequired(),
+                                 Length(min=4, max=30)
+                             ]
+                             )
+
+    last_name = StringField('Last Name',
+                            validators=[
+                                DataRequired(),
+                                Length(min=4, max=30)
+                            ]
+                            )
+    email = StringField('Email',
+                        validators=[
+                            DataRequired(),
+                            Email()
+                        ]
+                        )
+    password = PasswordField('Password',
+                             validators=[
+                                 DataRequired(),
+                             ]
+                             )
+    confirm_password = PasswordField('Confirm Password',
+                                     validators=[
+                                         DataRequired(),
+                                         EqualTo('password')
+                                     ]
+                                     )
+    submit = SubmitField('Sign Up')
+
+
+class LoginForm(FlaskForm):
+    email = StringField('Email',
+                        validators=[
+                            DataRequired(),
+                            Email()
+                        ])
+    password = PasswordField('Password',
+                             validators=[
+                                 DataRequired(),
+                             ])
+    remember = BooleanField('Remember Me')
+
+    submit = SubmitField('Login')
+
+
+class UpdateAccountForm(FlaskForm):
+    first_name = StringField('First Name',
+                             validators=[
+                                 DataRequired(),
+                                 Length(min=4, max=30)
+                             ]
+                             )
+
+    last_name = StringField('Last Name',
+                            validators=[
+                                DataRequired(),
+                                Length(min=4, max=30)
+                            ]
+                            )
+    email = StringField('Email',
+                        validators=[
+                            DataRequired(),
+                            Email()
+                        ]
+                        )
+    submit = SubmitField('Update')
